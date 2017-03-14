60a32d1f9f6027b063c07f2e89fc3490926956f0
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-03-14 13:49:45 +0300

    CMAS-316 Создать модули Nancy, DTO, логику создания работ

---
+++ Cmas.Backend.Modules.CallOffOrders/CallOffOrdersModule.cs
@@ -0,0 +1,11 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cmas.Backend.Modules.CallOffOrders
+{
+    public class CallOffOrdersModule : NancyModule
+    {
+    }
+}
